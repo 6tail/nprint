c5af63dd0ebbb4bb921c8115b5ced475c9584521
6tail <6tail@6tail.cn>
2018-07-31 14:06:30 +0800

    add more paper types

--- nprint.js
+++ nprint.js
@@ -27,7 +27,11 @@
     'A1':{width:594,height:841},
     'A2':{width:420,height:594},
     'A3':{width:297,height:420},
-    'A4':{width:210,height:297}
+    'A4':{width:210,height:297},
+    'A5':{width:148,height:210},
+    'B3':{width:353,height:500},
+    'B4':{width:250,height:353},
+    'B5':{width:176,height:250}
   };
   var ALIGN = ANCHOR;
   var _mask=null,_layer=null,_timer;
